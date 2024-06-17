2833d8ad2ec6c76deae717afe5729f8c4d4347fc
Pratik Patil <139785591+PratikSanjayPatil@users.noreply.github.com>
2024-06-17 19:17:16 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ Health.addEventListener("click",function options(e){
     if(count===1){
         e.preventDefault();
         healthOptions.style.display = "flex";
+        healthOptions.style.position = "fixed"
         icon.style.transform = "rotate(180deg)";
         icon.style.transition = "0.3s";
         icon.style.color = "#15ACA5";
@@ -256,6 +257,7 @@ const scrollslider = (leftv)=>{
 let leftv = 0
 
 if(screenWidth >= 720){
+    leftBTN.style.opacity = 0;
     rightBTN.addEventListener("click", function(){
     
         if(leftv>-600){
@@ -290,6 +292,7 @@ if(screenWidth >= 720){
 }
 
 else{
+    leftBTN.style.opacity = 0;
     rightBTN.addEventListener("click", function(){
     
         if(leftv>-1600){
@@ -324,10 +327,178 @@ else{
     })
 }
 
-// left.addEventListener("click",leftslide(p))
-
-
 // img slide show end
 
+// Shop by categories start
 
+let shop = document.querySelector("#shop");
+let shopInnerDiv = document.querySelector("#shop>.shopInnerDiv");
+let shopArr = document.querySelector("#shop>.shopInnerDiv>.shopArr")
+
+let shopList = [
+    {
+        img : "https://cdn01.pharmeasy.in/dam/discovery/categoryImages/f10917087a483040b557e4b18204312c.png?f=png?dim=256x0",
+        title : "Top products"
+    },
+    {
+        img : "https://cdn01.pharmeasy.in/dam/discovery/categoryImages/fa936f30b4563fc4abd187fb22fe5258.png?f=png?dim=256x0",
+        title : "Elderly care"
+    },
+    {
+        img : "https://cdn01.pharmeasy.in/dam/discovery/categoryImages/1e622b0308ec3ab48887512eaa3488a5.png?f=png?dim=256x0",
+        title : "Personal care"
+    },
+    {
+        img : "https://cdn01.pharmeasy.in/dam/discovery/categoryImages/9cc9a28ea4513009966cae794114eefd.png?f=png?dim=256x0",
+        title : "Healthcare devices"
+    },
+    {
+        img : "https://cdn01.pharmeasy.in/dam/discovery/categoryImages/e9ceb38252cd333f910b383ee75004c0.png?f=png?dim=256x0",
+        title : "Summer store"
+    },
+    {
+        img : "https://cdn01.pharmeasy.in/dam/discovery/categoryImages/ce48d6d71ace3892945f0b0a64c1f0e9.png?f=png?dim=256x0",
+        title : "Skin care"
+    },
+    {
+        img : "https://cdn01.pharmeasy.in/dam/discovery/categoryImages/11315fee93053980af48fd871bf687b8.png?f=png?dim=256x0",
+        title : "Mother and baby care"
+    },
+    {
+        img : "https://cdn01.pharmeasy.in/dam/discovery/categoryImages/4405b7900b0b34fcb5c8572aae9d4b2e.png?f=png?dim=256x0",
+        title : "Accessories & wearables"
+    },
+    {
+        img : "https://cdn01.pharmeasy.in/dam/discovery/categoryImages/514d0d7d01a63502b4ebfec9ae26f4d2.png?f=png?dim=256x0",
+        title : "Fitness supplements"
+    },
+    {
+        img : "https://cdn01.pharmeasy.in/dam/discovery/categoryImages/ecad9a974e003fb987858b3ee81413c6.png?f=png?dim=256x0",
+        title : "Ayurvedic care"
+    },
+    {
+        img : "https://cdn01.pharmeasy.in/dam/discovery/categoryImages/af7a9b53ad1239659615c6895b0972d7.png?f=png?dim=256x0",
+        title : "Sexual wellness"
+    },
+    {
+        img : "https://cdn01.pharmeasy.in/dam/discovery/categoryImages/105ceca515ee30509f93309ae75306aa.png?f=png?dim=256x0",
+        title : "Diabetic care"
+    },
+    {
+        img : "https://cdn01.pharmeasy.in/dam/discovery/categoryImages/35d6d4e00e5d3058906cfa8b92ca799c.png?f=png?dim=256x0",
+        title : "Health condition"
+    },
+    {
+        img : "https://cdn01.pharmeasy.in/dam/discovery/categoryImages/dc96175686f135b5a22d1e57165d0246.png?f=png?dim=256x0",
+        title : "Home care"
+    },
+    {
+        img : "https://cdn01.pharmeasy.in/dam/discovery/categoryImages/ef15f03b682d37c994eb978d6c85c455.png?f=png?dim=256x0",
+        title : "Trending products"
+    },
+    {
+        img : "https://cdn01.pharmeasy.in/dam/discovery/categoryImages/2e58770425cc39a983d6d71fb0cc07e2.png?f=png?dim=256x0",
+        title : "Surgicals and dressings"
+    }
+]
+
+let shopicon = document.querySelector(".shopicon")
+let sleft = document.querySelector(".shopicon>.sleft")
+let sright = document.querySelector(".shopicon>.sright")
+
+shopList.forEach(function(ele,i){
+    let shopDiv = document.createElement("div")
+    // let imgDiv = document.createElement("div")
+    // imgDiv.classList.add("shopimgDiv")
+    let img = document.createElement("img")
+    let p = document.createElement("p")
+
+    img.src = ele.img
+    // imgDiv.append(img)
+    p.innerHTML = ele.title
+    shopDiv.append(img,p)
+
+    shopArr.append(shopDiv)
+})
+
+
+const scrollslider2 = (left2)=>{
+    gsap.to("#shop>.shopInnerDiv>.shopArr>div",{
+        x:left2,
+        duration:1,
+    })
+}
+let left2 = 0
+
+if(screenWidth >= 720){
+    sleft.style.opacity = 0;
+    sright.addEventListener("click", function(){
+        
+        if(left2>-1200){
+            scrollslider2(left2-=200)
+            sleft.style.opacity = 10;
+        }
+        else if(left2 === -1200){
+            scrollslider2(left2-=180)
+            
+        }
+
+        if(left2===-1380){
+            sright.style.display = "none";
+        }
+    })
+    
+    
+    sleft.addEventListener("click", function(){
+        if(left2 === -180){
+            scrollslider2(left2+=180)
+            
+        }
+        else if(left2 <= -100){
+            scrollslider2(left2+=200)
+            sright.style.display = "flex";
+        }
+
+        if(left2===0){
+            sleft.style.opacity = 0;
+        }
+    })
+}
+
+else{
+    sleft.style.opacity = 0;
+    sright.addEventListener("click", function(){
+    
+        if(left2>-2200){
+            scrollslider2(left2-=200)
+            sleft.style.opacity = 100;
+        }
+        else if(left2 === -2200){
+            scrollslider2(left2-=35)
+            sright.style.display = "none";
+        }
+
+        if(left2 === -2235){
+            sright.style.display = "none";
+        }
+    })
+    
+    
+    sleft.addEventListener("click", function(){
+        if(left2 === -35){
+            scrollslider2(left2+=35)
+           
+        }
+        else if(left2 <= -100){
+            scrollslider2(left2+=200)
+            sright.style.display = "flex";
+            console.log(left2)
+        }
+
+        if(left2===0){
+            sleft.style.opacity = 0;
+        }
+    })
+}
+// Shop by categories end
 
